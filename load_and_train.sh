9349395cdae07c3d71cf0714df9813a5844cd8da
Sivakar Sithamparanathan <sivakar12@outlook.com>
2021-07-13 13:37:54 +0530

    Add shell script to run the ETL and training together

---
+++ load_and_train.sh
@@ -0,0 +1,4 @@
+rm data/DisasterResponse.db
+rm models/classifier.pkl
+python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db
+python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl
